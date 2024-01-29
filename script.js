3aec4cb038405a7d10ba17d1f2731ad604299206
GajendraKaushik <gajendrakaushik128@gmail.com>
2024-01-29 19:31:42 +0530

    Update: responsiveness

--- script.js
+++ script.js
@@ -3,52 +3,21 @@ const scroll = new LocomotiveScroll({
   smooth: true,
 });
 
-//  For the About and Past Experience Section
-
-var about_tablinks = document.getElementsByClassName("detail-links");
-var detail_contents = document.getElementsByClassName("about-contents");
-
-function selectTab(tabName) {
-  for (tab of about_tablinks) {
-    tab.classList.remove("active-link1");
-  }
-  for (tabcontent of detail_contents) {
-    tabcontent.classList.remove("active-tab1");
-  }
-  event.currentTarget.classList.add("active-link1");
-  document.getElementById(tabName).classList.add("active-tab1");
-}
-
 // For About Section Desc
-var tablinks = document.getElementsByClassName("tab-links-about");
+var tablinks = document.getElementsByClassName("tab-links-about1");
 var tabcontents = document.getElementsByClassName("tab-contents-about");
 
 function opentab(tabname) {
   for (tablink of tablinks) {
-    tablink.classList.remove("active-link-about");
+    tablink.classList.remove("active-link-about1");
   }
   for (tabcontent of tabcontents) {
     tabcontent.classList.remove("active-tab-about");
   }
-  event.currentTarget.classList.add("active-link-about");
+  event.currentTarget.classList.add("active-link-about1");
   document.getElementById(tabname).classList.add("active-tab-about");
 }
 
-// for Experience Desc
-
-var tablinks_exp = document.getElementsByClassName("tab-links-exp");
-var tabcontents_exp = document.getElementsByClassName("tab-contents-exp");
-
-function opentab_exp(tabname) {
-  for (tablink of tablinks_exp) {
-    tablink.classList.remove("active-link-exp");
-  }
-  for (tabcontent of tabcontents_exp) {
-    tabcontent.classList.remove("active-tab-exp");
-  }
-  event.currentTarget.classList.add("active-link-exp");
-  document.getElementById(tabname).classList.add("active-tab-exp");
-}
 
 
 /* ------Minicircle-------- */
